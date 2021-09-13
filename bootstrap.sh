a26d5de1f3059dc1ec17d955011f480f8f976d47
jampoyman <52150561+jampoyman@users.noreply.github.com>
2021-09-13 21:11:24 +0800

    change to fork shit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 SCRIPT_LOCATION=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname ${SCRIPT_LOCATION})
 PROJECT_PATH="${PROJECT_PATH:-$SCRIPT_PATH}"
-NGINX_VERSION=1.21.1
-NGINX_RTMP_MODULE_VERSION=1.2.2
+NGINX_VERSION=1.21.3
+NGINX_RTMP_MODULE_VERSION=dev
 NGINX_PATH=/usr/sbin/nginx                              # Make sure to change the init file too
 
 echo "Script location: ${SCRIPT_LOCATION}"
@@ -31,9 +31,10 @@ if [ ! -e $NGINX_PATH ]; then
     tar -zxvf nginx-${NGINX_VERSION}.tar.gz
     
     # Download Nginx's RTMP module used for live broadcasting
-    wget https://github.com/arut/nginx-rtmp-module/archive/v${NGINX_RTMP_MODULE_VERSION}.zip
+    
+    wget https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/refs/heads/dev.zip
     # Unzip the zip file
-    unzip v${NGINX_RTMP_MODULE_VERSION}.zip
+    unzip ${NGINX_RTMP_MODULE_VERSION}.zip
     cd nginx-rtmp-module-1.2.2
     find ./ -type f -exec sed -i 's/rtmp/pogi/g' {} \;
     mmv '*rtmp*' '#1pogi#2'
@@ -47,7 +48,7 @@ if [ ! -e $NGINX_PATH ]; then
     cd ..
 
     # Remove downloaded archives
-    rm v${NGINX_RTMP_MODULE_VERSION}.zip nginx-${NGINX_VERSION}.tar.gz
+    rm ${NGINX_RTMP_MODULE_VERSION}.zip nginx-${NGINX_VERSION}.tar.gz
 
     # Remove folder used to build Nginx
     rm -rf nginx-${NGINX_VERSION} nginx-rtmp-module-master
